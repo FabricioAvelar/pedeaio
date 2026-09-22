6e236e345feb01daa890a8e05e157c04fe908c74
Juan <juanbarrosp11@gmail.com>
2026-09-22 10:44:43 -0300

    Criação da aplicação Pedidos

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -8,14 +8,19 @@ class Usuario(AbstractUser):
     def __str__(self):
         return self.username
 
-# class Endereco(models.Model):
-#     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-#     rua = models.CharField(max_length=100)
-#     numero = models.CharField(max_length=10)
-#     cidade = models.CharField(max_length=100)
-#     estado = models.CharField(max_length=2)
-#     def __str__(self):
-#         return f'{self.rua}, {self.numero}'
+class Endereco(models.Model):
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        related_name='enderecos'
+    )
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.rua}, {self.numero} - {self.cidade}/{self.estado}'
 
 # class Favorito(models.Model):
 #     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
