4e5f250f00d43c8c6412cc167f476d598743807c
Juan <juanbarrosp11@gmail.com>
2026-09-22 11:20:53 -0300

    Implementação do EnderecoForm

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -1,17 +1,21 @@
 from django import forms
 from django.forms import ModelForm
-# from .models import (Usuario, Endereco, Favorito, Cupom, Produto)
-from .models import Usuario
+from .models import Usuario, Endereco
 
 class UsuarioForm(ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__'
 
-# class EnderecoForm(ModelForm):
-#     class Meta:
-#         model = Endereco
-#         fields = '__all__'
+class EnderecoForm(ModelForm):
+    class Meta:
+        model = Endereco
+        fields = [
+            'rua',
+            'numero',
+            'cidade',
+            'estado',
+        ]
 
 # class FavoritoForm(ModelForm):
 #     class Meta:
